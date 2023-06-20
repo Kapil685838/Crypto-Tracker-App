9eac42959193c549a99650bfbcf61203b81b4ffc
Kapil Chaudhary <kc685838@gmail.com>
2023-06-20 23:27:28 +0530

    updation in links

--- src/components/Common/Footer/index.js
+++ src/components/Common/Footer/index.js
@@ -19,7 +19,7 @@ function Footer() {
         <a href="https://facebook.com">
           <FacebookIcon className="social-link" />
         </a>
-        <a href="mailto:ahmeda.farhin786@gmail.com">
+        <a href="mailto:kc685838@gmail.com">
           <EmailIcon className="social-link" />
         </a>
         <a href="https://www.twitter.com">
